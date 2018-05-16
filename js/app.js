58fd045cd757e1380bb1b06294844593f17e7eb5
Katherina611 <katarzynag0609@gmail.com>
2018-05-16 14:59:01 +0200

    added menu

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//const
+const menuIcon = document.querySelector('.shop-menu li img');
+const menu = document.querySelectorAll('.shop-menu li a');
+const socialIcon = document.querySelectorAll('.social img');
+
+//event listeners
+menuIcon.addEventListener('click', showMenu);
+
+//functions
+function showMenu(){
+    menuIcon.style.display = 'none';
+    menu.forEach(li =>{
+        li.style.display = 'block'
+    });
+    socialIcon.forEach(img =>{
+        img.style.margin = '0 20px'
+    })
+}
