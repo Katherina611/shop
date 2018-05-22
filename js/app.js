9ca974500fe8fcb632e3bc13339b15c2ee34bf51
Katherina611 <katarzynag0609@gmail.com>
2018-05-22 11:02:58 +0200

    localStorage part1

--- js/app.js
+++ js/app.js
@@ -1,13 +1,48 @@
+//class
+class Shop {
+    saveChoco(chocolate){
+        let chocolates = this.getFromShop();
+        chocolates.push(chocolate);
+        localStorage.setItem('chocolates', JSON.stringify(chocolates));
+    }
+    getFromShop(){
+        let chocolates;
+        if(localStorage.getItem('chocolates') === null){
+            chocolates = [];
+        }else{
+            chocolates = JSON.parse(localStorage.getItem('chocolates'));
+        }
+        return chocolates;
+    }
+}
+
 //const
 const menuIcon = document.querySelector('.shop-menu li img');
 const menu = document.querySelectorAll('.shop-menu li a');
 const socialIcon = document.querySelectorAll('.social img');
+const basketBtn = document.querySelectorAll('.praline button');
+const shop = new Shop();
+
 
 //event listeners
 menuIcon.addEventListener('click', showMenu);
+basketBtn.forEach(choco =>{
+    choco.addEventListener('click', resultShop);
+});
 
 
 //functions
+function resultShop(event){
+    const cardChoco = event.target.parentElement;
+    const chocoInfo = {
+        name: cardChoco.querySelector('.pralines-name').textContent,
+        price:cardChoco.querySelector('.praline-price').textContent,
+        image:cardChoco.querySelector('.praline img').src
+    };
+    //add into the storage
+    shop.saveChoco(chocoInfo);
+}
+
 function showMenu(){
     menuIcon.style.display = 'none';
     menu.forEach(li =>{
@@ -17,4 +52,7 @@ function showMenu(){
         img.style.margin = '0 20px'
     })
 }
+
+
+
 
