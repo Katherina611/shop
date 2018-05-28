a4f7c1e90456fe65048a17c170226e01c0a8c7c5
Katherina611 <katarzynag0609@gmail.com>
2018-05-28 11:09:33 +0200

    select

--- js/app.js
+++ js/app.js
@@ -31,13 +31,23 @@ class Shop {
                 <td><img src='${choco.image}'></td>
                 <td><p>${choco.name}</p></td>
                 <td><p>${choco.price}</p></td>
+                <td><select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                </select></td>
                 <td><button class="btn-remove" data-name="${choco.name}">Remove</button></td>
             `;
             basket.appendChild(tr);
         });
     }
-
-
 }
 
 //const
